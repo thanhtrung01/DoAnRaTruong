949e0e6b1f3c519fc6eef8b052b4907d43863099
vithanhtu <vithanhtu21@gmail.com>
2023-03-07 21:18:03 +0700

    tu config layout

---
+++ front-end/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="navbar">Navbar</nav>
+      <div className="board-header">Board header</div>
+    </header>
+  );
+};
+
+export default Header;
