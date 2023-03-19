d0c6dc36101da609172555a047ff162d98ccd415
vithanhtu <vithanhtu21@gmail.com>
2023-03-19 10:38:58 +0700

    modal detail card

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
