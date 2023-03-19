033b54341469edaf9723652ab2c06d7ee0bb26a5
Nguyễn Thanh Trung <thanhtruongba2001@gmail.com>
2023-03-20 00:30:44 +0700

    add be login

--- front-end/src/redux/reducer/columnReducer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const columnReducer = () => {
-  return <div></div>;
-};
-
-export default columnReducer;
--- front-end/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import columnReducer from "./reducer/columnReducer";
-
-export const store = configureStore({
-  reducer: { columnReducer },
-});
