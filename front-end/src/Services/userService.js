3a5e6e2df9f0527df61b63d73482053edfaf9bfa
Nguyễn Thanh Trung <thanhtruongba2001@gmail.com>
2023-04-07 16:29:43 +0700

    push front-end 0.0.2

---
+++ front-end/src/Services/userService.js
@@ -0,0 +1,136 @@
+import axios from "axios";
+import Cookies from 'js-cookie';
+import {
+  registrationStart,
+  registrationEnd,
+  loginStart,
+  loginFailure,
+  loginSuccess,
+  loadSuccess,
+  loadFailure,
+  loadStart,
+  fetchingStart,
+  fetchingFinish,
+} from "../Redux/Slices/userSlice";
+import { openAlert } from "../Redux/Slices/alertSlice";
+import setBearer from "../Utils/setBearer";
+
+const apiURL = process.env.REACT_APP_SERVER_API;
+const authUrl = apiURL + `auth/`;
+const baseUrl = apiURL + `user/`;
+
+export const register = async (
+  { name, username, email, password, repassword },
+  dispatch
+) => {
+  dispatch(registrationStart());
+  if (password !== repassword) {
+    dispatch(
+      openAlert({
+        message: "Your passwords does not match!",
+        severity: "error",
+      })
+    );
+  } else {
+    try {
+      const res = await axios.post(`${authUrl}register`, {
+        name,
+        username,
+        email,
+        password,
+      });
+      dispatch(
+        openAlert({
+          message: res.data.message,
+          severity: "success",
+          nextRoute: "/login",
+          duration: 1500,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        openAlert({
+          message: error?.response?.data?.errMessage
+            ? error.response.data.errMessage
+            : error.message,
+          severity: "error",
+        })
+      );
+    }
+  }
+  dispatch(registrationEnd());
+};
+
+export const login = async ({ email, password }, dispatch) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post(authUrl + "login", { email, password });
+    const { user, message } = res.data;
+    // localStorage.setItem("token", user.token);
+    Cookies.set('token', user.token, { expires: 1/24 });
+    // localStorage.setItem('expirationDate', generateToken(expiresIn));
+    setBearer(user.token);
+      dispatch(loginSuccess({ user }));
+    dispatch(
+      openAlert({
+        message,
+        severity: "success",
+        duration: 500,
+        nextRoute: "/boards",
+      })
+    );
+  } catch (error) {
+    dispatch(loginFailure());
+    dispatch(
+      openAlert({
+        message: error?.response?.data?.errMessage
+          ? error.response.data.errMessage
+          : error.message,
+        severity: "error",
+      })
+    );
+  }
+};
+
+export const loadUser = async (dispatch) => {
+  dispatch(loadStart());
+  if (!Cookies.token) return dispatch(loadFailure());
+  setBearer(Cookies.token);
+  try {
+    const res = await axios.get(baseUrl + "get-user");
+    dispatch(loadSuccess({ user: res.data }));
+  } catch (error) {
+    dispatch(loadFailure());
+  }
+};
+
+export const getUserFromEmail = async (email, dispatch) => {
+  dispatch(fetchingStart());
+  if (!email) {
+    dispatch(
+      openAlert({
+        message: "Please write an email to invite",
+        severity: "warning",
+      })
+      );
+      dispatch(fetchingFinish());
+      return null;
+    }
+    
+  try {
+    const res = await axios.post(baseUrl + "get-user-with-email", { email });
+    dispatch(fetchingFinish());
+    return res.data;
+  } catch (error) {
+    dispatch(
+      openAlert({
+        message: error?.response?.data?.errMessage
+        ? error.response.data.errMessage
+        : error.message,
+        severity: "error",
+      })
+      );
+     dispatch(fetchingFinish());
+     return null;
+  }
+};
