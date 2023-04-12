0b90c09749427176ad45b7a4b474bee65633f087
vithanhtu <vithanhtu21@gmail.com>
2023-04-12 16:47:45 +0700

    handle expiredtoken

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
-import Index from './Components/Pages/IndexPage/Index';
-import Login from './Components/Pages/LoginPage/Login';
-import Register from './Components/Pages/RegisterPage/Register';
-import Alert from './Components/AlertSnackBar';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import Boards from './Components/Pages/BoardsPage/Boards';
-import ProtectedRoute from './Utils/ProtectedRoute';
-import { loadUser } from './Services/userService';
-import Store from './Redux/Store';
-import FreeRoute from './Utils/FreeRoute';
-import Board from './Components/Pages/BoardPage/Board';
-import Profile from './Components/Pages/ProfilePage/Profile';
+import React, { useEffect } from "react";
+import Index from "./Components/Pages/IndexPage/Index";
+import Login from "./Components/Pages/LoginPage/Login";
+import Register from "./Components/Pages/RegisterPage/Register";
+import Alert from "./Components/AlertSnackBar";
+import { BrowserRouter, Switch } from "react-router-dom";
+import Boards from "./Components/Pages/BoardsPage/Boards";
+import ProtectedRoute from "./Utils/ProtectedRoute";
+import { loadUser } from "./Services/userService";
+import Store from "./Redux/Store";
+import FreeRoute from "./Utils/FreeRoute";
+import Board from "./Components/Pages/BoardPage/Board";
+import Profile from "./Components/Pages/ProfilePage/Profile";
 const App = () => {
-	useEffect(() => {
-		loadUser(Store.dispatch);
-	}, []);
-	return (
-		<BrowserRouter>
-			<Alert />
-			<Switch>
-				<ProtectedRoute exact path="/boards" component={Boards} />
-				<ProtectedRoute exact path="/profile" component={Profile} />
-				<ProtectedRoute exact path="/board/:id" component={Board} />
-				<FreeRoute exact path="/login" component={Login} />
-				<FreeRoute exact path="/register" component={Register} />
-				<FreeRoute exact path="/" component={Index} />
-			</Switch>
-		</BrowserRouter>
-	);
+  useEffect(() => {
+    loadUser(Store.dispatch);
+  }, []);
+  return (
+    <BrowserRouter>
+      <Alert />
+      <Switch>
+        <ProtectedRoute exact path="/boards" component={Boards} />
+        <ProtectedRoute exact path="/profile" component={Profile} />
+        <ProtectedRoute exact path="/board/:id" component={Board} />
+        <FreeRoute exact path="/login" component={Login} />
+        <FreeRoute exact path="/register" component={Register} />
+        <FreeRoute exact path="/" component={Index} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
