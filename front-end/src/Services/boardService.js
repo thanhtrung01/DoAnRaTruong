464ae7e10517f2448d964b53a1ec2d280a39fca3
Nguyễn Thanh Trung <thanhtruongba2001@gmail.com>
2023-04-21 16:30:33 +0700

    test CI/CD v.0.0.1

--- front-end/src/Services/boardService.js
+++ front-end/src/Services/boardService.js
@@ -9,7 +9,7 @@ import {
 } from '../Redux/Slices/listSlice';
 import { openAlert } from '../Redux/Slices/alertSlice';
 import { addMembers, setActivityLoading, updateActivity, updateBackground, updateDescription } from '../Redux/Slices/boardSlice';
-
+ 
 const apiURL = process.env.REACT_APP_SERVER_API;
 const listRoute = apiURL + `list`;
 const boardRoute = apiURL + `board`;
